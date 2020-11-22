7ae81c6579c79a4d8e27a93b9db152f06f98b29f
Nombuso <38411152+Insitepass@users.noreply.github.com>
2020-11-22 23:05:26 +0200

    Update NoteList.dart

--- lib/Pages_Screens/NoteList.dart
+++ lib/Pages_Screens/NoteList.dart
@@ -1,16 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hazlo/Model/Note.dart';
 import 'package:hazlo/Pages_Screens/Calendar.dart';
 import 'package:hazlo/Pages_Screens/Note_details.dart';
 import 'package:hazlo/Services/db_service.dart';
 import 'package:hazlo/Widget/Note_item.dart';
-
+import 'package:hazlo/Widget/Popup_Menu.dart';
 import 'AddNote.dart';
+import 'Login.dart';
+import 'Settings.dart';
+import 'dart:async';
 
 class NoteList extends StatefulWidget {
+
   final Note note;
 
+
   const NoteList({Key key, this.note}) : super(key: key);
+
+
+
 
   @override
   State<StatefulWidget> createState() {
@@ -21,80 +31,116 @@ class NoteList extends StatefulWidget {
 
 class NoteListState extends State<NoteList> {
   int count = 0;
-
+   var list;
   //creating a list of items known as notes
   final notes = List<String>.generate(20, (i) => "Items ${i + 1}");
 
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  FirebaseUser user;
+
+
+  GoogleSignIn get _googleSignIn => GoogleSignIn();
   final _formKey = GlobalKey<FormState>();
+
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = new GlobalKey<
+      RefreshIndicatorState> ();
+
+
+  @override
+  void initState() {
+    super.initState();
+    list = List<String>.generate(20, (i) => "Items ${i + 1}");
+    _refresh();
+    initUser();
+  }
+
+  initUser() async {
+    user = await _firebaseAuth.currentUser();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     // ignore: non_constant_identifier_names
+
     return Scaffold(
         key: _formKey,
         backgroundColor: Colors.grey.shade300,
         appBar: AppBar(
-          title: Text('Notes'),
           automaticallyImplyLeading: false,
-          backgroundColor: Color(0xFF005792),
-          actions: <Widget>[
-             // user profile button
-            IconButton(
-              icon: Icon(Icons.person),
-              onPressed: () async {
-                getCurrentUser();
-              },
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Notes'),
+              // TODO   implement note Search  search
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
 
-            ),
+                },
+              ),
+            ],
+          ),
+          actions: <Widget>[
             // PopupMenu that has signout function
-           PopupMenuButton<String>(
-             onSelected: choiceAction,
-             itemBuilder: (BuildContext context) {
-               return Constants.choices.map((String choice) {
-               return PopupMenuItem<String>(
-                 value:  choice,
-                 child: Text(choice),
-               );
-               }).toList();
-             },
-           ),
+            PopupMenuButton<String>(
+              onSelected: choiceAction,
+              itemBuilder: (BuildContext context) {
+                return Constants.choices.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList();
+              },
+            ),
 
           ],
         ),
-            
-        body: StreamBuilder(
-          stream: notesDb.streamList(),
-          builder: (BuildContext context, AsyncSnapshot<List<Note>>snapshot) {
-            if (snapshot.hasError) return Center(
-              child: Text("There was an error"),
-            );
+        // Refresh indicator
+        body: RefreshIndicator(
+          key: _refreshIndicatorKey,
+          onRefresh: _refresh,
+          child:
+          StreamBuilder(
+            stream: notesDb.streamList(),
+            builder: (BuildContext context, AsyncSnapshot<List<Note>>snapshot) {
+              if (snapshot.hasError) return Center(
+                child: Text("Error: unable to loading your notes"),
+              );
 
-            if (!snapshot.hasData) return CircularProgressIndicator();
-            return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, index) {
-                  return NoteItem(note: snapshot.data[index],
-                    onDelete: (note)async {
-                    if(await _confrimDelete(context)) {
-                      notesDb.removeItem(note.id);
-                    }
-                    },
-                    onTap:(note) => Navigator.push(context,MaterialPageRoute(builder:(_)=> NoteDetails(note: note,),
-                    )
-                    ),
-                    onEdit: (note) {
-                    Navigator.push(context, MaterialPageRoute(
-                      builder:(_) => AddNote(note: note,)
-                    ));
-                    },
-                  );
-                }
-            );
-          },
+              if (!snapshot.hasData) return CircularProgressIndicator();
+              return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return NoteItem(note: snapshot.data[index],
+                      onDelete: (note) async {
+                        if (await _confrimDelete(context)) {
+                          notesDb.removeItem(note.id);
+                        }
+                      },
+                      onTap: (note) =>
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (_) => NoteDetails(note: note,),
+                          )
+                          ),
+                      onEdit: (note) {
+                        Navigator.push(context, MaterialPageRoute(
+                            builder: (_) => AddNote(note: note,)
+                        ));
+                      },
+                    );
+                  }
+              );
+            },
+          ),
         ),
+
+
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
+
             // FAB 2: Calender Button
             FloatingActionButton(
               child: Icon(Icons.date_range),
@@ -119,56 +165,119 @@ class NoteListState extends State<NoteList> {
               heroTag: null,
               backgroundColor: Color(0xFF005792),
 
-            )
+            ),
+
           ],
 
         )
-
     );
-
   }
-  
+
+// More menu Options
   Future<void> choiceAction(String choice) async
-{
-  if(choice == Constants.SignOut)
-    {
+  {
+    if (choice == Constants.SignOut) {
       await _firebaseAuth.signOut();
       await _googleSignIn.signOut();
 
       Navigator.push(context, new MaterialPageRoute(
           builder: (context) => new Login()));
-          print('user Signed Out');
+      print('user Signed Out');
     }
 
-}
-// getting the current user
-  Future<String> getCurrentUser() async {
-    final FirebaseUser user = await FirebaseAuth.instance.currentUser();
-    final String uid = user.uid.toString();
-    return uid;
+    if (choice == Constants.Settings) {
+      Navigator.push(
+          context, new MaterialPageRoute(builder: (context) => new Settings()));
+    }
   }
-  
-  
-  
-  
-  Future<bool>_confrimDelete(BuildContext context) async {
-   return showDialog(
-        context: context,
-       builder:(context) => AlertDialog(
-         title: Text("Confirm Delete"),
-         content: Text("Are you want to delete note?"),
-         actions: <Widget>[
-           FlatButton(
-             child:Text("No"),
-             onPressed: ()=> Navigator.pop(context,false) ,
-           ),
-           FlatButton(
-             child:Text("Yes"),
-             onPressed: ()=> Navigator.pop(context,true) ,
-           ),
-         ],
-       )
 
+
+  //Delete function
+  Future<bool> _confrimDelete(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) =>
+            AlertDialog(
+              title: Text("Confirm Delete"),
+              content: Text("Are you want to delete note?"),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("No"),
+                  onPressed: () => Navigator.pop(context, false),
+                ),
+                FlatButton(
+                  child: Text("Yes"),
+                  onPressed: () => Navigator.pop(context, true),
+                ),
+              ],
+            )
+
+    );
+  }
+
+  // pull to refresh function
+  Future<Null> _refresh() async {
+    _refreshIndicatorKey.currentState?.show(atTop: false);
+    await Future.delayed(Duration(seconds: 2));
+    setState(() {
+      list = List<String>.generate(20, (i) => "Items ${i + 1}");
+
+    });
+    return null;
+  }
+}
+
+//search function class
+class Search extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      IconButton(
+        icon: Icon(Icons.close),
+        onPressed: () {
+          query = "";
+        },
+
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  String selectedResult;
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container(
+        child: Center(
+          child: Text(selectedResult),
+        ),
+    );
+  }
+
+  final List<String>notelist;
+  final notes = List<String>.generate(20, (i) => "Items ${i + 1}");
+  Search(this.notelist);
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestedList = [];
+    query.isEmpty? suggestedList = notelist : suggestedList.addAll(notelist.where(
+        (element) => element.contains(query),
+    ));
+    IconButton(
+      onPressed: () {
+        showSearch(context: context, delegate: Search(notelist));
+      },
+      icon: Icon(Icons.search),
     );
   }
 }
