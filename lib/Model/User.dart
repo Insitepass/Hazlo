10f7389b25384640b4663cf33194e124629b3808
Nombuso <38411152+Insitepass@users.noreply.github.com>
2021-09-02 17:26:41 +0200

    Update User.dart

--- lib/Model/User.dart
+++ lib/Model/User.dart
@@ -2,6 +2,24 @@
 class User {
 
   final String uid;
+  final String name;
+  final String email;
+  final String  account;
 
-  User({this.uid});
+
+  User({this.uid,this.name,this.email,this.account,});
+  
+   User.fromDS(String id, Map<String, dynamic> data)
+      : uid = data['uid'],
+        name = data['Name'],
+        email = data ['Email'],
+        account = data['Account'];
+
+  Map<String, dynamic> toMap() => {
+
+    'uid': uid,
+    'name': name,
+    'email': email,
+    'account':account
+  };
 }
