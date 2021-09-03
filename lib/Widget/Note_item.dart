75ab7fb5de35ed60e616c34a5b7d1ddd91a2b8b2
Nombuso <38411152+Insitepass@users.noreply.github.com>
2021-09-03 02:18:35 +0200

    Update Note_item.dart

--- lib/Widget/Note_item.dart
+++ lib/Widget/Note_item.dart
@@ -1,16 +1,27 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hazlo/Model/Note.dart';
+import 'package:intl/intl.dart';
+import 'package:share/share.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
 
 class NoteItem extends StatelessWidget {
+
   final Note note;
   final Function(Note) onEdit;
   final Function(Note) onDelete;
+  //final Function(Note) onShare;
   final Function(Note) onTap;
+  final f = new DateFormat('yyyy-MM-dd hh:mm');
 
-  const NoteItem(
+
+
+   NoteItem(
       { Key key, @required this.note, @required this.onEdit, @required this.onDelete, @required this.onTap, t})
       : super (key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -24,26 +35,46 @@ class NoteItem extends StatelessWidget {
         actionPane: SlidableDrawerActionPane(),
         actionExtentRatio: 0.25,
         secondaryActions: <Widget>[
+          // Slide Edit Action
           IconSlideAction(
             caption: 'Edit',
             color: Colors.blueGrey,
             icon: Icons.edit,
             onTap: () => onEdit(note),
           ),
+
+          // Slide Delete Action
           IconSlideAction(
             caption: 'Delete',
             color: Colors.red,
             icon: Icons.delete,
             onTap: () => onDelete(note),
           ),
+
+        //Slide Share Action
+          IconSlideAction(
+            caption: 'Share',
+            color: Color(0xFF005792),
+            icon: Icons.share,
+            onTap: () => onShare(context,note),
+          ),
         ],
+
         child: ListTile(
           onTap: () => onTap(note),
           title: Text(note.title),
+          // displaying when the note was created.
+          subtitle:Text(timeago.format(DateTime.tryParse(note.createdAt.toString())).toString())
         ),
       ),
-
-
     );
   }
+  // share functionality.
+  void onShare(BuildContext context, Note note) {
+   final String  text =  "Hazlo:" " ${note.title} - ${ note.description}";
+
+    Share.share(text,subject: note.description);
+
+  }
+
 }
