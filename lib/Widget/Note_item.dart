aa15993ec762f663cf906e16ad5dc9be2aac4ef6
Nombuso <38411152+Insitepass@users.noreply.github.com>
2021-09-04 02:16:47 +0200

    Update Note_item.dart

--- lib/Widget/Note_item.dart
+++ lib/Widget/Note_item.dart
@@ -1,16 +1,29 @@
+
+import 'package:favorite_button/favorite_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hazlo/Model/Note.dart';
+import 'package:intl/intl.dart';
+import 'package:share/share.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
 
 class NoteItem extends StatelessWidget {
+
   final Note note;
   final Function(Note) onEdit;
   final Function(Note) onDelete;
+  //final Function(Note) onShare;
   final Function(Note) onTap;
+  final f = new DateFormat('yyyy-MM-dd hh:mm');
+  final  bool isStarred = false;
 
-  const NoteItem(
+
+
+   NoteItem(
       { Key key, @required this.note, @required this.onEdit, @required this.onDelete, @required this.onTap, t})
       : super (key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -24,26 +37,58 @@ class NoteItem extends StatelessWidget {
         actionPane: SlidableDrawerActionPane(),
         actionExtentRatio: 0.25,
         secondaryActions: <Widget>[
+          // Slide Edit Action
           IconSlideAction(
             caption: 'Edit',
             color: Colors.blueGrey,
             icon: Icons.edit,
             onTap: () => onEdit(note),
           ),
+
+          // Slide Delete Action
           IconSlideAction(
             caption: 'Delete',
             color: Colors.red,
             icon: Icons.delete,
             onTap: () => onDelete(note),
           ),
+
+        //Slide Share Action
+          IconSlideAction(
+            caption: 'Share',
+            color: Color(0xFF005792),
+            icon: Icons.share,
+            onTap: () => onShare(context,note),
+          ),
         ],
+
         child: ListTile(
+          //TODO maintain favorite state
+          leading: StarButton(
+            isStarred:false,
+            iconSize: 40,
+            iconColor: Colors.blueGrey,
+            valueChanged:(_isStarred) {
+              print('Is Starred : $_isStarred');
+
+            }
+          ),
           onTap: () => onTap(note),
           title: Text(note.title),
+          // displaying when the note was created.
+          subtitle:Text(timeago.format(DateTime.tryParse(note.createdAt.toString())).toString()),
+
+
+          )
         ),
-      ),
-
-
     );
   }
+  // share functionality.
+  void onShare(BuildContext context, Note note) {
+   final String  text =  "Hazlo:" " ${note.title} - ${ note.description}";
+
+    Share.share(text,subject: note.description);
+
+  }
+
 }
